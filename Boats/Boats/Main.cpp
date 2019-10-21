a0e026caaf1f806acbc57cc7c55321273d73c52a
Bazzan <sebake01@gmail.com>
2019-10-22 00:07:21 +0200

    Update Main.cpp

--- Boats/Boats/Main.cpp
+++ Boats/Boats/Main.cpp
@@ -8,8 +8,8 @@ int main() {
 	Boat* theBoats[3];
 
 	theBoats[0] = new Boat("Octo", 105);
-	theBoats[1] = new SailingBoat("Pussy", 23, false);
-	theBoats[2] = new SailingBoat("xxx", 8, false);
+	theBoats[1] = new SailingBoat("Cat", 23, false);
+	theBoats[2] = new SailingBoat("Dog", 8, false);
 	for (int i = 0; i < 3; i++) {
 		printBoat(theBoats[i]);
 	}
